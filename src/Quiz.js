4ea7c6e67a5d8c0bb60daced321915822853bddf
Kevin Glessner <kglessner@selectintl.com>
2018-06-18 10:38:18 -0400

    Display quiz questions and handle events

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,12 @@ class Quiz extends Component {
     super(props);
     this.state = { quiz_position: 1 };
   }
+
+  showNextQuestion() {
+    console.log('showNextQuestion');
+    this.setState({ quiz_position: this.state.quiz_position + 1 });
+  }
+
   render() {
     const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
     return (
@@ -17,6 +23,7 @@ class Quiz extends Component {
           <QuizEnd />
         ) : (
           <QuizQuestion
+            showNextQuestionHandler = {() => this.showNextQuestion()}
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,15 +2,27 @@ import React, { Component } from "react";
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
+
+  handleClick(buttonText) {
+    if (this.props.quiz_question.answer === buttonText) {
+      this.props.showNextQuestionHandler();
+    }
+  }
+
   render() {
+
+    const answers = this.props.quiz_question.answer_options.map((value, index) => 
+      <QuizQuestionButton onClick={(button_text) => this.handleClick(button_text)} key={index} button_text={value}/>
+    );
+
     return (
       <main>
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
-        <section className="buttons">
+        <section className="buttons">          
           <ul>
-              <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+            {answers}
           </ul>
         </section>
       </main>
